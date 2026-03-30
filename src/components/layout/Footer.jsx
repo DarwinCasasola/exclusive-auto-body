891fb17890f9cdbb0bfd0e1bafe6224433304675
DarwinCasasola14 <you@example.com>
2026-03-30 16:47:15 -0700

    updated styles

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+      <footer className="footer">
+        <div className="footer__container">
+          <p>© {new Date().getFullYear()} Exclusive Auto Body. All rights reserved.</p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;
